6b2a3afcebbd99317fce3e4ab926fadf235c64f0
Gunnar Þórunnarson <gus@tskoli.is>
2022-10-23 19:26:07 +0000

    Update 2_template_dictionary.py

--- 2-Flask/Templates/2_template_dictionary.py
+++ 2-Flask/Templates/2_template_dictionary.py
@@ -1,14 +1,5 @@
-# sækjum einnig render_template
 from flask import Flask, render_template
 app = Flask(__name__)
-
-"""
-Simple Jinja template use case.
-The render_template() function invokes the Jinja template engine that comes bundled with the Flask framework. 
-Jinja substitutes {{ ... }} blocks with the corresponding values, given by the arguments provided in the render_template() call.
-HTML special characters are escaped automatically to prevent XSS attacks.
-
-"""
 
 @app.route('/')
 def index():
@@ -19,7 +10,6 @@ def index():
     # sendum dictionary (user) og breytuna (title) í template
     # skilum html (index.html) sem er vistuð í templates möppu (þurfum ekki að vísa í hana) 
     return render_template('template2.html', title=title, user=user)
-
 
 if __name__ == '__main__':
   app.run(debug=True, use_reloader=True)  
