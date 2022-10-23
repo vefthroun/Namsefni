811328319cebf22ead93cab902ded4080b9cec7a
Gunnar Þórunnarson <gus@tskoli.is>
2022-10-23 21:41:55 +0000

    Update 1_template_static.py

--- 2-Flask/Templates/1_template_static.py
+++ 2-Flask/Templates/1_template_static.py
@@ -1,6 +1,11 @@
 from flask import Flask
-# The render_template() function invokes the Jinja template engine that comes bundled with the Flask framework. 
 from flask import render_template
+"""
+The render_template() function invokes the Jinja template engine that comes bundled with the Flask framework. 
+Jinja substitutes {{ ... }} blocks with the corresponding values, given by the arguments provided in the render_template() call.
+HTML special characters are escaped automatically to prevent XSS attacks.
+"""
+
 
 app = Flask(__name__)
 
@@ -9,7 +14,7 @@ def index():
     # skilum html (index.html) sem er vistuð í templates möppu (þurfum ekki að vísa í hana sérstaklega) 
     return render_template("template1.html")
   
-
+    
 if __name__ == '__main__':
   app.run(debug=True, use_reloader=True)  
     
