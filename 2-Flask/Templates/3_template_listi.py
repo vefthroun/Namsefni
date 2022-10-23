100822a41cc8b136d38b75f8bb0ea8bf23240fbf
Gunnar Þórunnarson <gus@tskoli.is>
2022-10-23 21:21:40 +0000

    Create 3_template_listi.py

---
+++ 2-Flask/Templates/3_template_listi.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    users = ['Gunnar', 'Daníel']
+    return render_template('template1.html', users=users)
+
+if __name__ == '__main__':
+  app.run(debug=True, use_reloader=True)  
+    
