baf28006d3c6d2a529126a018d613840b880ae2b
Gunnar Þórunnarson <gus@tskoli.is>
2022-10-23 23:21:49 +0000

    Update and rename include.py to 10_include.py

--- 2-Flask/Templates/10_include.py
+++ 2-Flask/Templates/10_include.py
@@ -1,13 +1,9 @@
-# sækjum einnig render_template
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 """
-Include
-https://jinja.palletsprojects.com/en/2.11.x/templates/#include
-
-Önnur leið til að búta niður vef í endurnýtanlega skráarparta.
+Include er Leið til að búta niður vef í endurnýtanlega skráarparta.
+https://jinja.palletsprojects.com/en/3.0.x/templates/#include
 
 {% include 'header.html' %}
     Body
@@ -15,7 +11,6 @@ https://jinja.palletsprojects.com/en/2.11.x/templates/#include
 """
 
 @app.route('/')
-@app.route('/index')
 def index():
     user = {'username': 'guest'}
     return render_template('include.html', title='Home', user=user)
