31512117f7b3efd8dd0b536ea2104d83aaade887
Gunnar Þórunnarson <gus@tskoli.is>
2022-10-23 21:22:21 +0000

    Update 3_template_listi.py

--- 2-Flask/Templates/3_template_listi.py
+++ 2-Flask/Templates/3_template_listi.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     users = ['Gunnar', 'Daníel']
-    return render_template('template1.html', users=users)
+    return render_template('template3.html', users=users)
 
 if __name__ == '__main__':
   app.run(debug=True, use_reloader=True)  
