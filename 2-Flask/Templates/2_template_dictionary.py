a78fcf4e1cb27673dbc4f47331a6dcfc8b8b0a59
Gunnar Þórunnarson <gus@tskoli.is>
2022-10-23 20:24:32 +0000

    Update 2_template_dictionary.py

--- 2-Flask/Templates/2_template_dictionary.py
+++ 2-Flask/Templates/2_template_dictionary.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+"""
+Jinja substitutes {{ ... }} blocks with the corresponding values, given by the arguments provided in the render_template() call.
+HTML special characters are escaped automatically to prevent XSS attacks.
+"""
+
 @app.route('/')
 def index():
     # data
