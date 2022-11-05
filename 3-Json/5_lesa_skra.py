6ca71a25df499f4b8078706770d5d54afb0499f0
Gunnar Þórunnarson <gus@tskoli.is>
2022-11-05 21:21:07 +0000

    Update 5_lesa_skra.py

--- 3-Json/5_lesa_skra.py
+++ 3-Json/5_lesa_skra.py
@@ -1,11 +1,11 @@
 from flask import Flask, json
 app = Flask(__name__)
 
-# Tengjumst við skránna 5.json (verður að vera til)
-with open("C:/Users/gjg/Desktop/vefforitun1/Namsefni/3-Json/5.json", encoding='utf-8') as skra:
+# Tengjumst við skránna 5.json (verður að vera til) sem við hýsum á Github 
+with open("slóðáskrá.json", encoding='utf-8') as skra:
     gogn = json.load(skra) # sækjum data úr Json skrá
 
-# Prentum nafnið Þórður, gogn = dictionary 
+# Prentum nafnið Gunnar, gogn = dictionary 
 print(gogn["nemandi"][0]["nafn"])
 
 @app.route('/')
