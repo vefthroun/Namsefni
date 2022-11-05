c5abcb4c28b9db5fc7a92eb18550c1f3fde8eb7e
Gunnar Þórunnarson <gus@tskoli.is>
2022-11-05 22:45:39 +0000

    Update 5_lesa_skra.py

--- 3-Json/5_lesa_skra.py
+++ 3-Json/5_lesa_skra.py
@@ -1,16 +1,20 @@
 from flask import Flask, json
 app = Flask(__name__)
 
-# Tengjumst við skránna 5.json (verður að vera til) sem við hýsum á Github 
-with open("slóðáskrá.json", encoding='utf-8') as skra:
+# Við getum búið til JSON skrá í sömu möppu Hýsum JSON á Github.
+# Gists: https://miguelpiedrafita.com/articles/github-gists (max 60 request pr klst nema notað sé token). 
+# Vistaðu skránna með .JSON sniðmáti.
+# veldu `raw` og afritaðu vefslóð (url) og bættu við .json við endingu 
+
+with open("https://gist.githubusercontent.com/GunnarThorunnarson/13839d272efae357adc1edbcd91f2938/raw/5faf23d220437b74adc73a94ba3edd2a0e01a8c2/nemendur.json", encoding='utf-8') as skra:
     gogn = json.load(skra) # sækjum data úr Json skrá
 
-# Prentum nafnið Gunnar, gogn = dictionary 
-print(gogn["nemandi"][0]["nafn"])
+# Prentum nafnið Þórður, gogn = dictionary 
+print(gogn["nemendur"][0]["nafn"])
 
 @app.route('/')
 def index():
-    return gogn["nemandi"][0]["nafn"] # Þórður
+    return gogn["nemendur"][0]["nafn"] # Þórður
 
 if __name__ == '__main__':
   app.run(debug=True, use_reloader=True)  
