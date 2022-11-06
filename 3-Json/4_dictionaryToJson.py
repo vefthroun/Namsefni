3959bd675d0c81307554e99aeef3a5ed9576116a
Gunnar Þórunnarson <gus@tskoli.is>
2022-11-06 22:04:55 +0000

    Update 4_dictionaryToJson.py

--- 3-Json/4_dictionaryToJson.py
+++ 3-Json/4_dictionaryToJson.py
@@ -9,8 +9,7 @@ d = {
 
 # convert dictionary to JSON (string containing JSON)
 # dumps fyrir strengi, dump fyrir skrár
-# indent=2 til að fá læsilegt format
-json_string_data = json.dumps(d, indent=2)
+json_string_data = json.dumps(d) 
 
 # '{"first_name": "Guido", "last_name": "Rossum", "titles": ["BDFL", "Developer"]}'
 print(json_string_data)
