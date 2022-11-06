290aded3af9ffde601f41ad2c27a9549115daf90
Gunnar Þórunnarson <gus@tskoli.is>
2022-11-06 11:19:52 +0000

    Update 5_lesa_skra.py

--- 3-Json/5_lesa_skra.py
+++ 3-Json/5_lesa_skra.py
@@ -1,17 +1,8 @@
-from flask import Flask, json
-app = Flask(__name__)
+import json
 
-# Tengjumst við skránna 5.json (verður að vera til)
-# Opening JSON file with open('sample.json', 'r') 
+# Opnum JSON skránna 5.json verður að vera til
 with open("5.json", encoding='utf-8') as skra:
     gogn = json.load(skra) # sækjum data úr Json skrá
 
 # Prentum nafnið Þórður, gogn = dictionary 
 print(gogn["nemandi"][0]["nafn"])
-
-@app.route('/')
-def index():
-    return gogn["nemandi"][0]["nafn"] # Þórður
-
-if __name__ == '__main__':
-  app.run(debug=True, use_reloader=True)  
