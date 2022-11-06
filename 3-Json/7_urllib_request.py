b8beb6f56da03356d83fc35e7722372e8cae93f6
Gunnar Þórunnarson <gus@tskoli.is>
2022-11-06 21:47:31 +0000

    Update 7_urllib_request.py

--- 3-Json/7_urllib_request.py
+++ 3-Json/7_urllib_request.py
@@ -16,8 +16,8 @@ print(gogn["nemendur"][0]["nafn"])
 
 
 """
+# Annað sýnidæmi
 import urllib.request
-
 # sækjum source code frá python.org
 with urllib.request.urlopen('http://python.org/') as response:
    print(response.read()) # skoðum í terminal
