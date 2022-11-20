5cb1cdf5249df86ea2b404613311cb790781b5c6
Gunnar Þórunnarson <gus@tskoli.is>
2022-11-20 13:24:31 +0000

    Update 1_auth.py

--- 6-Gagnagrunnur/Firebase/1_auth.py
+++ 6-Gagnagrunnur/Firebase/1_auth.py
@@ -1,20 +1,39 @@
+
+# https://github.com/nhorvath/Pyrebase4
+# þarf pip3 install pyrebase4 
 import pyrebase
 
-
-# tengin við firebase realtime database á firebase.google.com 
-# hér kemur tengingin þín við Firebase gagnagrunninn ( realtime database )
-config = {
-    "apiKey": "Setja ykkar eigin KEY",
-    "authDomain": "verkefni6-4265f.firebaseapp.com",
-    "projectId": "verkefni6-4265f",
-    "storageBucket": "verkefni6-4265f.appspot.com",
-    "messagingSenderId": "746762172223",
-    "appId": "1:746762172223:web:4bb950f0fa7b8d1168058a",
-    "measurementId": "G-PBS5F76K22",
-    "databaseURL": "https://NAFN Á Gagnagrunni.firebaseio.com",
+# stillingar til að tengjast firebase realtime database á firebase.google.com 
+firebaseConfig = {
+  "apiKey": "Setja ykkar eigin KEY",
+  "authDomain": "vefur3.firebaseapp.com",
+  "databaseURL": "https://NAFN Á Gagnagrunni.firebaseio.com",
+  "projectId": "vefur3",
+  "storageBucket": "vefur3.appspot.com",
+  "messagingSenderId": "723537595408",
+  "appId": "1:723537595408:web:13bd005e43576b1b318e95"
 }
 
-fb = pyrebase.initialize_app(config)
-auth = fb.auth()
-user = auth.create_user_with_email_and_password("test5@tskoli.is","1234567")
-print(user)
+#  init app með tengingu við gagnagrunn 
+firebase=pyrebase.initialize_app(firebaseConfig)
+
+# Authentication: Build -> Authentication á https://console.firebase.google.com/
+# authentication á user í fb.
+auth=firebase.auth()
+
+# login
+email=input("enter email")
+password=input("enter password")
+
+# login validation þar sem einn notandi er þegar til í fb.
+try:
+    user = auth.sign_in_with_email_and_password(email, password) 
+    print("Successfully signed in!")
+    print(user)
+except:
+    print("Invalid user or password. Try Again!")
+
+    
+# Að búa til user með python, pyrebase
+# user = auth.create_user_with_email_and_password("test@tskoli.is","1234567")
+# print(user)
