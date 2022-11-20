cafba20b483ee7012295388309d55c012fa4fe8b
Gunnar Þórunnarson <gus@tskoli.is>
2022-11-20 13:05:21 +0000

    Update app_fb_read_write.py

--- 6-Gagnagrunnur/Firebase/app_fb_read_write.py
+++ 6-Gagnagrunnur/Firebase/app_fb_read_write.py
@@ -6,28 +6,27 @@ app = Flask(__name__)
 # tengin við firebase realtime database á firebase.google.com 
 config = {
     # hér kemur tengingin þín við Firebase gagnagrunninn ( realtime database )
-
 }
-
 fb = pyrebase.initialize_app(config)
+
 db = fb.database()
 
 # Test route til að setja gögn í db
-@app.route('/')
+@app.route('/skrifa')
 def index():
+    # skrifum nýjan í grunn hnútur sem heitir notandi 
+    db.child("notandi").push({"notendanafn":"user", "lykilorð":psw)}) 
     return "Skrifað í grunn"
+
 
 # Test route til að sækja öll gögn úr db
 @app.route('/lesa')
-def lesa():
-    return "Lesum úr grunni"
+def lesa():	
+   # förum í grunn og sækjum allar raðir ( öll gögn )
+   response = db.child("notandi").get().val()  # fáum raðað dictionary í response
+   data = list(response.items()) 	       # breytum í lista með tuples
+   return "Lesum úr grunni"
 
 if __name__ == "__main__":
 	app.run(debug=True)
 
-# skrifum nýjan í grunn hnútur sem heitir notandi 
-# db.child("notandi").push({"notendanafn":"user", "lykilorð":psw)}) 
-
-# # förum í grunn og sækjum allar raðir ( öll gögn )
-# u = db.child("notandi").get().val()
-# lst = list(u.items())
