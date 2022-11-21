8f44820ab7c59323c29467b244a17918eff0dfa4
Gunnar Þórunnarson <gus@tskoli.is>
2022-11-21 09:39:56 +0000

    Update auth_flask.py

--- 6-Gagnagrunnur/Firebase/auth_flask.py
+++ 6-Gagnagrunnur/Firebase/auth_flask.py
@@ -1,13 +1,15 @@
 from flask import Flask, session
-import pyrebase, os
+import os
+# https://github.com/nhorvath/Pyrebase4
+import pyrebase   # pip3 install pyrebase4 
 
 app = Flask(__name__)
 
-# session lykill
+# used to sign session cookies for protection against cookie data tampering.
 app.secret_key = os.urandom(8)
 
-# tenging við Firebase gagnagrunninn 
-config = {
+# stillingar til að tengjast firebase realtime database á firebase.google.com 
+firebaseConfig = {
     "apiKey": "NOTA YKKAR KEY",
     "authDomain": "verkefni6-4265f.firebaseapp.com",
     "projectId": "verkefni6-4265f",
@@ -18,31 +20,60 @@ config = {
     "databaseURL": "https://NAFN Á GAGNAGRUNNI.firebaseio.com",
 }
 
-fb = pyrebase.initialize_app(config)
+fb = pyrebase.initialize_app(firebaseConfig)
+
+# pyrebase: https://github.com/nhorvath/Pyrebase4#authentication
 auth = fb.auth()
+
+# Get a reference to the database service
+db = fb.database()
+
 
 @app.route('/signup')
 def signup():
+    name = "Gunnar Þórunnarson"
+    display_name = "Gunni"
+    photo_url = "slóð á ljósmynd af mér"
+    email = "dummy2@example.is"
+    password = "1234567"
+
     try:
-        user = auth.create_user_with_email_and_password("email@example.is","1234567")
+        # Creating users
+        # https://github.com/nhorvath/Pyrebase4#creating-users
+       
+        user = auth.create_user_with_email_and_password(email, password)
+        # print(user) # localId er id á user fyrir auth.
+       
+        # Add user info
+        # user = auth.update_profile(display_name, photo_url, delete_attribute)
+    
+        # userdata to save in db
+        data = {
+            "name": name,
+            "email": email,
+            "avatar": display_name,
+            "selfie": photo_url
+        }
+        # write userdata to db.
+        db.child("users").push(data)
+
     except:
         print("could not create user")
     return "Athugaðu færslu"
-
 
 @app.route('/login')
 def login():
     try:
         # To sign in user using email and password
-        sign_user = auth.sign_in_with_email_and_password("email@example.is", "1234567")
+        user = auth.sign_in_with_email_and_password("email@example.is", "1234567")
         
-        # before the 1 hour expiry:
-        sign_user = auth.refresh(sign_user['refreshToken'])
+        # A user's idToken expires after 1 hour
+        user = auth.refresh(user['refreshToken'])  # sjá líka Custom tokens
         # now we have a fresh token
-        print(sign_user['idToken'])      
+        print(user['idToken'])      
        
-        session['user'] = sign_user['idToken']
-        print(session['user'])
+        # færum id auðkenni í session
+        session['user'] = user['idToken']
         
         print("sign In Successfull")
     except:
